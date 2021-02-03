cd4f69b1e0bb56784e251c9eb07a9939f6ca6532
applesoftsol <63569208+applesoftsol@users.noreply.github.com>
2021-02-03 09:22:45 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,10 @@
+provider "azurerm" {
+  version = "=2.4.0"
+
+  subscription_id = "395b2b6c-8fb6-4cc1-83e4-39455be6b102"
+  client_id       = "72b2db2e-be38-456a-90ba-6f56877fc782"
+  client_secret   = "pq064c.VJ5_X_a~rwTDKs8In42eQhmmw67"
+  tenant_id       = "90c3f360-0a02-49e4-b70c-4ebb69378edf"
+
+  features {}
+}
