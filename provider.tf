f134a35abc22d34f012e31a623ab0600942e3191
m.Chaitanya <67100925+chaitanya2020j@users.noreply.github.com>
2023-09-05 18:17:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "=2.4.0"
+  version = "3.50.0"
 
   subscription_id = "395b2b6c-8fb6-4cc1-83e4-39455be6b102"
   client_id       = "72b2db2e-be38-456a-90ba-6f56877fc782"
