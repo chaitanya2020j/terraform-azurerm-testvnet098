47c3ed9ca81b9b54a2e0b18aa4714106a77d86db
m.Chaitanya <67100925+chaitanya2020j@users.noreply.github.com>
2023-09-05 21:08:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 
   subscription_id = "8d71aebc-1c66-46d7-a194-333fceea9b80"
   client_id       = "a811df8b-ce68-4695-af20-94815e303231"
-  client_secret   = "c10b1311-cd35-4eeb-ba3a-e177ef23ee03"
+  client_secret   = "LL18Q~yxSJFfxgIDfp6-PCnEW5jGTzhg6zo8PaJv"
   tenant_id       = "7dc07ee4-bf07-4a8e-aef4-7d7dd586f13f"
 
   features {}
